57fc64bd6c7cf5695d63f1a1fd3fc95553243f76
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-09-04 19:51:15 -0400

    Registro Usuarios

--- Entidades/Usuarios.cs
+++ Entidades/Usuarios.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,29 @@ namespace Entidades
 {
     public class Usuarios
     {
+        [Key]
+        public int UsuarioId { get; set; }
+        public String Nombre { get; set; }
+        public String Clave { get; set; }
+        public DateTime Fecha { get; set; }
+        public String NivelUsuario { get; set; }
+
+        public Usuarios(int usuarioId, string nombre, string clave, DateTime fecha, string nivelUsuario)
+        {
+            UsuarioId = usuarioId;
+            Nombre = nombre;
+            Clave = clave;
+            Fecha = fecha;
+            NivelUsuario = nivelUsuario;
+        }
+
+        public Usuarios()
+        {
+            UsuarioId = 0;
+            Nombre = String.Empty;
+            Clave = String.Empty;
+            Fecha = DateTime.Now;
+            NivelUsuario = String.Empty;
+        }
     }
 }
---
+++ EntidadesTests/UsuariosTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Tests
+{
+    [TestClass()]
+    public class UsuariosTests
+    {
+        [TestMethod()]
+        public void UsuariosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UsuariosTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
